c2b42e4dc7ee865c5bc844d0aa3158ba97e264cf
jbeiran <josemibeiran@gmail.com>
2024-10-16 00:28:35 +0100

    feature: add constructor in Extra class

--- src/org/ulpgc/is1/model/Extra.java
+++ src/org/ulpgc/is1/model/Extra.java
@@ -2,5 +2,10 @@ package org.ulpgc.is1.model;
 
 public abstract class Extra {
     public int dayPrice;
+
+    public Extra(int dayPrice) {
+        this.dayPrice = dayPrice;
+    }
+
     public abstract String getName();
 }
